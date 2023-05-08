ff9b23e9c27a8d5377442f97074c03af2ea8d432
ktullavik <ktullavik@gmail.com>
2023-05-09 00:15:25 +0200

    Simplify.

--- src/main.rs
+++ src/main.rs
@@ -143,148 +143,60 @@ fn main() {
 
 
     fn read_inputfile(s: &str) -> String {
-        let testfile: &str;
 
-        match s {
-            "1" => {
-                testfile = "1.hello.dart";
-            }
-            "2" => {
-                testfile = "2.variable.dart";
-            }
-            "3" => {
-                testfile = "3.addition.dart";
-            }
-            "4" => {
-                testfile = "4.subtraction.dart";
-            }
-            "5" => {
-                testfile = "5.multiplication.dart";
-            }
-            "6" => {
-                testfile = "6.division.dart";
-            }
-            "7" => {
-                testfile = "7.funcall.dart";
-            }
-            "8" => {
-                testfile = "8.argpass.dart";
-            }
-            "9" => {
-                testfile = "9.evaled_argpass.dart";
-            }
-            "10" => {
-                testfile = "10.arithmetic.dart";
-            }
-            "11" => {
-                testfile = "11.conditional.dart";
-            }
-            "12" => {
-                testfile = "12.conditional2.dart";
-            }
-            "13" => {
-                testfile = "13.conditional3.dart";
-            }
-            "14" => {
-                testfile = "14.conditional4.dart";
-            }
-            "15" => {
-                testfile = "15.conditional5.dart";
-            }
-            "16" => {
-                testfile = "16.mutate.dart";
-            }
-            "17" => {
-                testfile = "17.mutate_self.dart";
-            }
-            "18" => {
-                testfile = "18.post_increment.dart";
-            }
-            "19" => {
-                testfile = "19.post_decrement.dart";
-            }
-            "20" => {
-                testfile = "20.pre_increment.dart";
-            }
-            "21" => {
-                testfile = "21.pre_decrement.dart";
-            }
-            "22" => {
-                testfile = "22.returnvalue.dart";
-            }
-            "23" => {
-                testfile = "23.logical_or.dart";
-            }
-            "24" => {
-                testfile = "24.logical_and.dart";
-            }
-            "25" => {
-                testfile = "25.logical_expr.dart";
-            }
-            "26" => {
-                testfile = "26.less_than.dart";
-            }
-            "27" => {
-                testfile = "27.greater_than.dart";
-            }
-            "28" => {
-                testfile = "28.less_or_equal.dart";
-            }
-            "29" => {
-                testfile = "29.greater_or_equal.dart";
-            }
-            "30" => {
-                testfile = "30.equality.dart";
-            }
-            "31" => {
-                testfile = "31.equality2.dart";
-            }
-            "32" => {
-                testfile = "32.equality3.dart";
-            }
-            "33" => {
-                testfile = "33.recursion.dart";
-            }
-            "34" => {
-                testfile = "34.unary_minus.dart";
-            }
-            "35" => {
-                testfile = "35.arg_expression.dart";
-            }
-            "36" => {
-                testfile = "36.arg_expression2.dart";
-            }
-            "37" => {
-                testfile = "37.arg_expression3.dart";
-            }
-            "38" => {
-                testfile = "38.arg_expression4.dart";
-            }
-            "39" => {
-                testfile = "39.not.dart";
-            }
-            "40" => {
-                testfile = "40.fibonacci.dart";
-            }
-            "41" => {
-                testfile = "41.difficult_return.dart";
-            }
+        let filename = match s {
+            "1" => "1.hello.dart",
+            "2" => "2.variable.dart",
+            "3" => "3.addition.dart",
+            "4" => "4.subtraction.dart",
+            "5" => "5.multiplication.dart",
+            "6" => "6.division.dart",
+            "7" => "7.funcall.dart",
+            "8" => "8.argpass.dart",
+            "9" => "9.evaled_argpass.dart",
+            "10" => "10.arithmetic.dart",
+            "11" => "11.conditional.dart",
+            "12" => "12.conditional2.dart",
+            "13" => "13.conditional3.dart",
+            "14" => "14.conditional4.dart",
+            "15" => "15.conditional5.dart",
+            "16" => "16.mutate.dart",
+            "17" => "17.mutate_self.dart",
+            "18" => "18.post_increment.dart",
+            "19" => "19.post_decrement.dart",
+            "20" => "20.pre_increment.dart",
+            "21" => "21.pre_decrement.dart",
+            "22" => "22.returnvalue.dart",
+            "23" => "23.logical_or.dart",
+            "24" => "24.logical_and.dart",
+            "25" => "25.logical_expr.dart",
+            "26" => "26.less_than.dart",
+            "27" => "27.greater_than.dart",
+            "28" => "28.less_or_equal.dart",
+            "29" => "29.greater_or_equal.dart",
+            "30" => "30.equality.dart",
+            "31" => "31.equality2.dart",
+            "32" => "32.equality3.dart",
+            "33" => "33.recursion.dart",
+            "34" => "34.unary_minus.dart",
+            "35" => "35.arg_expression.dart",
+            "36" => "36.arg_expression2.dart",
+            "37" => "37.arg_expression3.dart",
+            "38" => "38.arg_expression4.dart",
+            "39" => "39.not.dart",
+            "40" => "40.fibonacci.dart",
+            "41" => "41.difficult_return.dart",
 
-            "50" => {
-                testfile = "50.classdef.dart";
-            }
+            "50" => "50.classdef.dart",
 
-            "70" => {
-                testfile = "70.list_replace.dart";
-            }
-            s => {
-                testfile = s;
-            }
-        }
+            "70" => "70.list_replace.dart",
+
+            s => s
+        };
 
         let mut input = String::new();
-        let mut f = File::open(format!("{}{}", TESTPATH, testfile)).expect("file not found");
-        f.read_to_string(&mut input).expect("Test file not found.");
+        let mut f = File::open(format!("{}{}", TESTPATH, filename)).expect("File not found!");
+        f.read_to_string(&mut input).expect("Error when reading input file.");
         return input;
     }
 
