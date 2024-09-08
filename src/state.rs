65a8b48a3543e970845131752d6be147271d4717
ktullavik <ktullavik@gmail.com>
2024-09-08 19:13:37 +0200

    Comment.

--- src/state.rs
+++ src/state.rs
@@ -5,7 +5,10 @@ use node::Node;
 use std::collections::HashMap;
 
 
-
+// globals is where all the top level nodes (functions, constructors)
+// are actually stored. looktables allows, given a filename, to look
+// up the top level nodes that are accessible by the file. It gives
+// an index into globals.
 pub struct State {
     pub filepath: String,
     pub globals: Vec<Node>,
