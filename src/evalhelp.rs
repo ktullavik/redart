d1542c84c9634fd20af3f76328a0a04ee9ab05d0
ktullavik <ktullavik@gmail.com>
2024-10-01 12:46:10 +0200

    Remove debug prints.

--- src/evalhelp.rs
+++ src/evalhelp.rs
@@ -3,8 +3,7 @@ use crate::{object::Object, state::State, utils::dart_evalerror};
 
 
 pub fn set_list_element(ulist_ref: Object, index: Object, value: Object, state: &mut State) -> Object {
-    
-    println!("set_list_element");
+
     match ulist_ref {
 
         Object::Reference(ulist_rk) => {
@@ -15,20 +14,17 @@ pub fn set_list_element(ulist_ref: Object, index: Object, value: Object, state: 
             if let Object::Reference(ilist_rk) = ilist_ref {
                 let ilist = state.objsys.get_list_mut(&ilist_rk);
 
-
                 if let Object::Int(i) = index {
                     if i < 0 {
                         dart_evalerror("Index must be positive.", state)
                     }
 
-                    println!("Setting internal list element {}", value);
                     ilist.set_el(i as usize, value);
                     return Object::Null;
                 }
             }
             panic!("Expected reference when setting list element.")
         }
-
         x => panic!("Unexpected token: {}", x)
     }
 }
